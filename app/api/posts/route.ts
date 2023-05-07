aff58d031655be4c6a1790742c101bdac6229fb1
tHyt-lab <washers.fezzes-0q@icloud.com>
2023-05-07 08:54:14 +0000

    fix: page,api

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,12 +1,11 @@
 import { prisma } from "@/lib/prisma";
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export const GET: NextApiHandler = async function (req: NextApiRequest, res: NextApiResponse) {
+export const GET = async function (req: Request) {
   try {
     const data = await prisma.post.findMany()
 
-    return NextResponse.json({ data, ddd: 123 }, {status: 200});
+    return NextResponse.json({ data, ddd: 123 }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 })
   }
